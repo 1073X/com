c17e99e72706149fb9ee4199235437918b70ed87
Joe Wu <wuzongsheng@gmail.com>
2021-01-04 00:40:41 +0800

    replace fatal_error with runtime_error

--- include/com/fatal_error.hpp
+++ include/com/fatal_error.hpp
@@ -5,22 +5,11 @@
 #include "datetime.hpp"
 #include "strcat.hpp"
 
-namespace miu::com {
-
-class fatal_error : public std::runtime_error {
-  public:
-    fatal_error() = delete;
-    template<typename... ARGS>
-    fatal_error(ARGS&&... args)
-        : std::runtime_error(
-            strcat { std::forward<ARGS>(args)..., strcat::delimiter { " " } }.str()) {}
-};
-
-}    // namespace miu::com
-
 template<typename... ARGS>
 void
-FATAL_ERROR(ARGS&&... args) {
+FATAL_ERROR(ARGS&&... args) {    // NOLINT
     auto time = miu::com::datetime::now();
-    throw miu::com::fatal_error { "FATAL", time, std::forward<ARGS>(args)... };
+    auto delimiter = miu::com::strcat::delimiter { " " };
+    auto msg = miu::com::strcat { "FATAL", time, args..., delimiter };
+    throw std::runtime_error { msg.str() };
 }
--- unittest/ut_fatal_error.cpp
+++ unittest/ut_fatal_error.cpp
@@ -2,22 +2,12 @@
 
 #include "com/fatal_error.hpp"
 
-TEST(ut_fatal_error, what) {
-    miu::com::fatal_error err { "message" };
-    EXPECT_STREQ("message", err.what());
-}
-
-TEST(ut_fatal_error, shortcut) {
-    EXPECT_THROW(FATAL_ERROR("message"), miu::com::fatal_error);
+TEST(ut_fatal_error, throw_runtime_error) {
     EXPECT_THROW(FATAL_ERROR("message"), std::runtime_error);
-}
-
-TEST(ut_fatal_error, concat) {
     try {
         FATAL_ERROR(1, "xyz");
-    } catch (miu::com::fatal_error const& err) {
+    } catch (std::runtime_error const& err) {
         auto msg = err.what() + 31;    // skip the timestamp
         EXPECT_STREQ("1 xyz", msg);
     }
 }
-
