a9a1fcacf6d42fda21f1da1fd573a2406d150974
Joe Wu <wuzongsheng@gmail.com>
2021-01-12 19:49:01 +0800

    accept different type of exception

--- unittest/ut_fatal_error.cpp
+++ unittest/ut_fatal_error.cpp
@@ -11,3 +11,13 @@ TEST(ut_fatal_error, throw_runtime_error) {
         EXPECT_STREQ("1 xyz", msg);
     }
 }
+
+TEST(ut_fatal_error, throw_logic_error) {
+    EXPECT_THROW(FATAL_ERROR<std::logic_error>("message"), std::logic_error);
+    try {
+        FATAL_ERROR<std::logic_error>(1, "xyz");
+    } catch (std::logic_error const& err) {
+        auto msg = err.what() + 31;    // skip the timestamp
+        EXPECT_STREQ("1 xyz", msg);
+    }
+}
