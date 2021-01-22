e8a07212dba085c66ce62a911d3a7f87c1b8245d
Joe Wu <wuzongsheng@gmail.com>
2021-01-22 19:47:50 +0800

    to_string

--- source/lib/days.cpp
+++ source/lib/days.cpp
@@ -7,3 +7,12 @@ namespace miu::com {
 static_assert(sizeof(int32_t) == sizeof(days));
 
 }
+
+namespace std {
+
+std::string
+to_string(miu::com::days val) {
+    return std::to_string(val.count()) + "d";
+}
+
+}    // namespace std
