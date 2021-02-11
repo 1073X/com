251b9c34d8206f8aace5bed448c00947f4bb310e
Joe Wu <wuzongsheng@gmail.com>
2021-02-11 07:41:52 +0000

    Resolve "impl metadata"

--- unittest/ut_meta_info.cpp
+++ unittest/ut_meta_info.cpp
@@ -12,3 +12,8 @@ TEST(ut_meta_info, info) {
     EXPECT_STREQ("test.unittest.ut_com", miu::meta::info());
 }
 
+TEST(ut_meta_info, overflow) {
+    EXPECT_ANY_THROW(miu::meta::set_category("0123456789abcdef"));
+    EXPECT_ANY_THROW(miu::meta::set_type("0123456789abcdef"));
+    EXPECT_ANY_THROW(miu::meta::set_name("0123456789abcdef"));
+}
